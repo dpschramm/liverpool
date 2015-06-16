6dd810df86bba6b6a94598641adf7bfdf603dc10
Daniel Schramm <dpschramm@gmail.com>
2015-06-16 18:04:23 +1000

    Update URL parameters as date value is different

--- scraper.rb
+++ scraper.rb
@@ -1,4 +1,4 @@
 require 'scraperwiki'
 require File.dirname(__FILE__) + '/lib_icon_rest_xml/scraper'
 
-scrape_icon_rest_xml("http://eplanning.liverpool.nsw.gov.au/Pages/XC.Track/SearchApplication.aspx", "d=last14days&k=LodgementDate&t=290&o=xml")
+scrape_icon_rest_xml("http://eplanning.liverpool.nsw.gov.au/Pages/XC.Track/SearchApplication.aspx", "?d=thismonth&k=LodgementDate&o=xml")
